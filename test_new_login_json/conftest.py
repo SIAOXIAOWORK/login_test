468f835eafbb0b7254da229c47df66db0ed4dee3
SIAOXIAOWORK <qas910195@gmail.com>
2025-04-22 09:21:30 +0100

    Fix CI: run : pytest4

--- test_new_login_json/conftest.py
+++ test_new_login_json/conftest.py
@@ -6,7 +6,7 @@ def browser_page(request):
    
     browser_name = request.param    
     with sync_playwright() as p :
-        browser = getattr(p,browser_name).launch(headless = False)
+        browser = getattr(p,browser_name).launch()
         context = browser.new_context()
         page = context.new_page()
         yield {"page": page, "browser_name": browser_name}
